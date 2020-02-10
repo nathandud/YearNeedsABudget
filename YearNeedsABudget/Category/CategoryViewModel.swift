3ee23729653f10995d50d91fee437b08a7ab394b
Nathan Dudley <nathantdudley@gmail.com>
2020-02-10 00:21:44 -0600

    Create models and organize view controller

---
+++ YearNeedsABudget/Category/CategoryViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CategoryViewModel.swift
+//  YearNeedsABudget
+//
+//  Created by Nathan Dudley on 2/9/20.
+//  Copyright © 2020 Nathan Dudley. All rights reserved.
+//
+
+import Foundation
+
+struct CategoryViewModel {
+    
+}
