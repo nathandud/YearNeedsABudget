364f9f0788ddf6b711488ed6ead18647f47d96fa
Nathan Dudley <dudleynathan@byu.net>
2020-04-19 02:35:04 -0500

    Get monthly budget summaries and aggregate into annual category summaries

--- YearNeedsABudget/CategoryAnnual/CategoryAnnualSummary.swift
+++ YearNeedsABudget/CategoryAnnual/CategoryAnnualSummary.swift
@@ -14,16 +14,51 @@ struct CategoryAnnualSummary {
     let name: String
     let budgeted: Int
     let activity: Int
-    let balance: Int
-    let months: [MonthlySummary]
+    let months: [CategoryMonthlySummary]
     
-    init(categoryId: String, categoryGroupId: String, name: String, budgeted: Int, activity: Int, balance: Int, months: [MonthlySummary]) {
+    init(categoryId: String, categoryGroupId: String, name: String, months: [CategoryMonthlySummary]) {
+        self.categoryId = categoryId
+        self.categoryGroupId = categoryGroupId
+        self.name = name
+        self.budgeted = months.reduce(0) { (result, monthlySummary) -> Int in return result + monthlySummary.budgeted  }
+        self.activity = months.reduce(0) { $0 + $1.activity }
+        self.months = months
+    }
+}
+
+struct CategoryMonthlySummary {
+    let month: Int
+    let categoryId: String
+    let categoryGroupId: String
+    let name: String
+    let budgeted: Int
+    let activity: Int
+    
+    init(month: Int, categoryId: String, categoryGroupId: String, name: String, budgeted: Int, activity: Int) {
+        self.month = month
         self.categoryId = categoryId
         self.categoryGroupId = categoryGroupId
         self.name = name
         self.budgeted = budgeted
         self.activity = activity
-        self.balance = balance
-        self.months = months
+    }
+}
+
+struct CategoryAggregator {
+    static func aggregate(monthlyBudgetSummaries: [MonthlyBudgetSummary]) -> [CategoryAnnualSummary] {
+        
+        let categoryMonthlySummaries = monthlyBudgetSummaries.map { (monthlySummary) -> [CategoryMonthlySummary] in
+            guard let categories = monthlySummary.categories, let month = YnabDateFormatter.shared.getMonthNumber(from: monthlySummary.month) else { return [] }
+            return categories.map { (category) -> CategoryMonthlySummary in
+                return CategoryMonthlySummary(month: month, categoryId: category.id, categoryGroupId: category.categoryGroupId, name: category.name, budgeted: category.budgeted, activity: category.activity)
+            }
+        }.flatMap { $0 }
+        
+        let groupedMonthlySummaries = Dictionary(grouping: categoryMonthlySummaries, by: { $0.categoryId })
+        
+        return groupedMonthlySummaries.compactMap { (categorySummaries) -> CategoryAnnualSummary? in
+            guard let firstSummary = Array(categorySummaries.value).first else { return nil }
+            return CategoryAnnualSummary(categoryId: categorySummaries.key, categoryGroupId: firstSummary.categoryGroupId, name: firstSummary.name, months: categorySummaries.value)
+        }
     }
 }
